d0520d9a81470c439617d99ae463f631e41349e1
mohammedtouheed7 <mohammedtouheed75@gmail.com>
2021-08-20 19:57:44 +0530

    add 17 new line

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ $c_date_string =strtotime($c_date);
 // $date = DateTime::createFromFormat($format, $date1);
 // $newtime = $date->format('H:i:s A') . "\n";
 var_dump($c_date);
+
+echo "hello github";
 // var_dump($c_time);
 // var_dump($c_date_string);
 
